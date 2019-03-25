eb0a51c15827c91fb0742514d7608db9139bc5c2
Tara-Gabriella Engelhardt <taraengy@gmail.com>
2019-03-25 18:50:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-  //write fetch request to the Game of Thrones API
+  fetch('https://anapioficeandfire.com/api/characters')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
